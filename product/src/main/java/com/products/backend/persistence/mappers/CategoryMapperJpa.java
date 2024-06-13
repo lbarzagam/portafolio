92d3c6c7688e71d9b1e7ef0e3aafc28dd15b5d85
Lorenzo Bárzaga Meriño <lbarzagam11@gmail.com>
2024-06-13 15:24:29 -0500

    Delete product directory
    Se movió para otro directorio

--- product/src/main/java/com/products/backend/persistence/mappers/CategoryMapperJpa.java
+++
@@ -1,9 +0,0 @@
-package com.products.backend.persistence.mappers;
-
-import com.products.backend.domain.entities.Category;
-import com.products.backend.persistence.entities.CategoryJpa;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface CategoryMapperJpa extends BaseJpaMapper<Category, CategoryJpa> {
-}
--- product/src/main/java/com/products/backend/persistence/mappers/ProductMapperJpa.java
+++
@@ -1,9 +0,0 @@
-package com.products.backend.persistence.mappers;
-
-import com.products.backend.domain.entities.Product;
-import com.products.backend.persistence.entities.ProductJpa;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface ProductMapperJpa extends BaseJpaMapper<Product, ProductJpa> {
-}
