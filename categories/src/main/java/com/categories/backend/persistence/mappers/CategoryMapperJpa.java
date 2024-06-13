6eaa44c1d7e680ad323d87893d6b01e279f2f05b
Lorenzo Bárzaga Meriño <lbarzagam11@gmail.com>
2024-06-13 15:21:46 -0500

    Delete categories directory
    Se movio para el directorio Obtener Datos Api

--- categories/src/main/java/com/categories/backend/persistence/mappers/CategoryMapperJpa.java
+++
@@ -1,9 +0,0 @@
-package com.categories.backend.persistence.mappers;
-
-import com.categories.backend.domain.entities.Category;
-import com.categories.backend.persistence.entities.CategoryJpa;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface CategoryMapperJpa extends BaseJpaMapper<Category, CategoryJpa> {
-}
